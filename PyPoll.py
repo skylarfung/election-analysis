55a52d404558bd7768bfeada584de5b02eba43de
Skylar Fung <skylarfung@outlook.com>
2020-02-02 19:09:38 -0800

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -13,16 +13,63 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#initalize varables
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0 
+winning_percentage = 0
+
+
 #open the election results and read the file
 with open(file_to_load) as election_data:
-
-    #To do: perform analysis
     file_reader = csv.reader(election_data)
 
-    #print each row in CSV file
+    #read header row
     headers = next(file_reader)
-    print(headers)
 
+    #print each row in the CSV file
+    for row in file_reader:
+
+        #gets total votes
+        total_votes += 1
+
+        #prints the candidate names        
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+
+            #track the candidate votes
+            candidate_votes[candidate_name] = 0
+
+        #add votes for candidates
+        candidate_votes[candidate_name] += 1
+
+    #determine the percentage of votes for each candidate
+    for candidate in candidate_votes:
+        votes = candidate_votes[candidate]
+        vote_percentage = float(votes) / float(total_votes) * 100
+        print(f"{candidate}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        #determines winning candidate
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate
+
+#summary
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
+
+
+    
 
 
 #using the with statement open the file as a text file
